d6783ea99bd775156e068d56245392947268b9e7
max-queiroz <142455640+max-queiroz@users.noreply.github.com>
2024-02-26 22:03:45 -0300

    adicionar arquivos do ex2

---
+++ av2/src/main/java/ifpr/av2/RequestServlet.java
@@ -0,0 +1,36 @@
+package ifpr.av2;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/requisicao")
+public class RequestServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        PrintWriter writer = resp.getWriter();
+        String metodo = req.getMethod();
+        String uri = req.getRequestURI();
+        String protocolo = req.getProtocol();
+        String endereco = req.getRemoteAddr();
+
+        writer.println("<html>");
+
+        writer.println("<p>" + metodo + "</p>");
+        writer.println("<p>" + uri + "</p>");
+        writer.println("<p>" + protocolo + "</p>");
+        writer.println("<p>" + endereco + "</p>");
+
+        writer.println("</html>");
+
+
+
+    }
+}
